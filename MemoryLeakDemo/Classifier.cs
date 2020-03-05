38c6e59881a3526a4e6ee64c8052a231636540b4
venki <a123920@id.amada.biz>
2020-03-05 13:33:15 +0900

    Initial commit.

---
+++ MemoryLeakDemo/Classifier.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using System.Security;
+using Microsoft.ML.Transforms.Image;
+using Microsoft.ML.Transforms.Onnx;
+
+namespace MemoryLeakDemo
+{
+    class Classifier
+    {
+        private MLContext ML;
+        private ITransformer model;
+        private string inputName = "input_2";
+        private string outputName = "dense_2";
+
+        public Classifier()
+        {
+            ML = new MLContext();
+
+            var imageLoader = ML.Transforms.LoadImages(inputName, string.Empty, nameof(InputData.ImagePath));
+            var imageResizer = ML.Transforms.ResizeImages(inputName, 320, 240);
+            var pixelExtractor = ML.Transforms.ExtractPixels(inputName, interleavePixelColors: true, colorsToExtract: ImagePixelExtractingEstimator.ColorBits.Red);
+            var onnxEstimator = ML.Transforms.ApplyOnnxModel(new string[] { outputName }, new string[] { inputName }, "Model.onnx");
+
+            var pipeline = imageLoader.Append(imageResizer).Append(pixelExtractor).Append(onnxEstimator);
+
+            var data = ML.Data.LoadFromEnumerable(new List<InputData>());
+            model = pipeline.Fit(data);
+        }
+
+
+        public void ClassifyImage(string imagePath)
+        {
+            var inputData = new InputData()
+            {
+                ImagePath = imagePath
+            };
+
+            var data = ML.Data.LoadFromEnumerable(new List<InputData>() { inputData });
+
+            var result = model.Transform(data);
+
+            //IEnumerable<float[]> probs = result.GetColumn<float[]>(outputName);
+        }
+    }
+
+    class InputData
+    {
+        public string ImagePath;
+    }
+}
---
+++ MemoryLeakDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace MemoryLeakDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Classifier classifier = new Classifier();
+            Console.WriteLine("Starting initial operation.");
+            Console.WriteLine("Run 1");
+
+            for (int i = 0; i < 25; i++)
+            {
+                classifier.ClassifyImage("Image.png");
+            }
+
+            Console.WriteLine("Create snapshot and press enter.");
+            Console.ReadLine();
+            Console.WriteLine("Run 2");
+
+            for (int i = 0; i < 25; i++)
+            {
+                classifier.ClassifyImage("Image.png");
+            }
+
+            Console.WriteLine("Create second snapshot and press enter to exit.");
+            Console.ReadLine();
+        }
+    }
+}
